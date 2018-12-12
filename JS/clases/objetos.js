ff202d33436b8263231e26032c3571589e2a9fa9
unknown <adry.solfer@gmail.com>
2018-12-12 19:18:28 +0100

    npm y css

---
+++ JS/clases/objetos.js
@@ -0,0 +1,13 @@
+//Usuarios
+var usr1= new usuario("Artyomて",954511371,"reguleros");
+var usr2 = new usuario("Paco",954511372,"ninguno");
+
+//Home
+var home1 = new home(nombre,cuentas,carrosDeCombate);
+
+//Carros de combate
+var leopard1 = new carrosDeCombate("Leopard 1","Alemania",1965,"Tanque Medio(MBT)");
+var t55 = new carrosDeCombate("T-55","URSS",1955,"Tanque Medio");
+var m1_abrams = new carrosDeCombate("M1 Abrams","EEUU",1980,"CCP")
+
+//consoleLogs
